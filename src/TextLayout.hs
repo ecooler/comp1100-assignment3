24b38a73e839ea37c94f122f10260dce7c327755
powcoder代写 <powcoder@163.com>
2022-07-09 17:16:28 +0800

    程序代写 加微信 powcoder

---
+++ src/TextLayout.hs
@@ -0,0 +1,18 @@
+module TextLayout where
+
+import Data.List (intercalate)
+
+-- | Draw a grid of strings.
+grid :: [[String]] -> String
+grid table = concatMap renderRow table
+  where
+    renderRow row = "| " ++ intercalate " | " (paddedRow row) ++ " |\n"
+    nCols = maximum (map length table)
+
+    columnWidths :: [Int]
+    columnWidths = take nCols (foldr merge (repeat 0) table) where
+      merge row = zipWith max (map length row ++ repeat 0)
+
+    paddedRow row
+      = zipWith padTo columnWidths (row ++ replicate (nCols - length row) "")
+    padTo n cell = cell ++ replicate (n - length cell) ' '
